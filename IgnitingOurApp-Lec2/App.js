51784c60c13ab8c0004216e47dd10066670c0d7f
Suyog28 <suyogvmuley@gmail.com>
2023-08-10 22:47:44 +0530

    IgnitingApp Code Updated

---
+++ IgnitingOurApp-Lec2/App.js
@@ -0,0 +1,17 @@
+const parent = React.createElement("div", { id: "parent" },
+    [React.createElement("div", { id: "child" },
+        [React.createElement("h1", {}, "This h1 from parent"),
+        React.createElement("h2", {}, "This h2 from parent")],
+        "Namaste React Inception Episode 1")], [
+    React.createElement("div", { id: "red" }, "Namaste React Inception Episode 1")
+],
+    [React.createElement("div", { id: "child" },
+        [React.createElement("h1", {}, "This h1 from parent"),
+        React.createElement("h2", {}, "This h2 from parent")],
+        "Namaste React Inception Episode 1")], [
+    React.createElement("div", { id: "red" }, "Namaste React Inception Episode 1")
+]);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
