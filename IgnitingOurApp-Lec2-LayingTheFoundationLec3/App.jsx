8f71e0a4cc101806cab6b8fd4df3dd0872865afd
Suyog28 <suyogvmuley@gmail.com>
2023-08-13 20:46:45 +0530

    React Element and React Component basic

--- IgnitingOurApp-Lec2-LayingTheFoundationLec3/App.jsx
+++ IgnitingOurApp-Lec2-LayingTheFoundationLec3/App.jsx
@@ -2,6 +2,27 @@ import React from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 //JSX => React.createElement => ReactElement-JS Object => HTMLElement(render)
-const heading = <h1> Namaste React- JSX is not a HTMl but HTML like Syntax</h1>;//JSX added
+
+//React Element
+
+const headingelement = <h1>Hello Element in react</h1>;
+
+const Heading = () =>
+(
+    <h1> Namaste React- JSX is not a HTMl but HTML like Syntax</h1>//JSX added
+);
+
+//React Component
+//Class Based Component - OLD
+//Functional Component - NEW
+
+const HeadingComponent = () => {
+    return <div className='container'>
+        {headingelement} {/*React Element */}
+        <Heading /> {/*Functional Component */}
+        <h1>Namste React Functional Component</h1>
+    </div>
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent />);
