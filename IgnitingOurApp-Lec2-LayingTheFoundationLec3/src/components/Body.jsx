f893a1ce31aa0eb3bb71c86870bd0ccecb7244b4
Suyog28 <suyogvmuley@gmail.com>
2023-08-20 00:21:30 +0530

    Top rating filter added using useState Hook

--- IgnitingOurApp-Lec2-LayingTheFoundationLec3/src/components/Body.jsx
+++ IgnitingOurApp-Lec2-LayingTheFoundationLec3/src/components/Body.jsx
@@ -1,17 +1,25 @@
+import { useState } from 'react';
 import ResCard from '../components/ResCard';
-
 import resList from '../utils/mockData';
 
 const Body = () => {
+    const [listOfRest, setlistOfRest] = useState(resList);
+
     return (
         <div className='body-container' >
             <div className='search'>
                 <input type='text' name='search' className='searchinput' placeholder='Search'></input> Search
             </div>
+            <div className='filter-btn'>
+                <button className='top-btn'
+                    onClick={() => {
+                        const filterData = listOfRest.filter((res) => res.info.avgRating > 4);
+                        setlistOfRest(filterData);
+                    }}>Top Rating</button>
+            </div>
             <div className='res-container'>
-                {/* <ResCard resname={resList[2]} /> */}
                 {
-                    resList.map((resitems) => <ResCard resData={resitems} key={resitems.info.id} />)
+                    listOfRest.map((resitems) => <ResCard resData={resitems} key={resitems.info.id} />)
                 }
             </div>
         </div>
