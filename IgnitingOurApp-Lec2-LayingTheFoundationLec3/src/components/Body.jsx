9449b5b82ab143296eafc553407dd450b2ce753d
Suyog28 <suyogvmuley@gmail.com>
2023-08-20 00:50:32 +0530

    Search Filter Added using useState

--- IgnitingOurApp-Lec2-LayingTheFoundationLec3/src/components/Body.jsx
+++ IgnitingOurApp-Lec2-LayingTheFoundationLec3/src/components/Body.jsx
@@ -3,12 +3,26 @@ import ResCard from '../components/ResCard';
 import resList from '../utils/mockData';
 
 const Body = () => {
+
     const [listOfRest, setlistOfRest] = useState(resList);
+    const [searchInput, setSearchInput] = useState("")
 
     return (
         <div className='body-container' >
             <div className='search'>
-                <input type='text' name='search' className='searchinput' placeholder='Search'></input> Search
+                <input type='text' name='search'
+                    className='searchinput'
+                    placeholder='Search'
+                    value={searchInput}
+                    onChange={(e) =>
+                        setSearchInput(e.target.value)}
+                />
+                <button className='search-btn'
+                    onClick={() => {
+                        const searchData = listOfRest.filter((res) => res.info.name.includes(searchInput));
+                        setlistOfRest(searchData);
+                    }}
+                >Search</button>
             </div>
             <div className='filter-btn'>
                 <button className='top-btn'
